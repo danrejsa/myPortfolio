e8a7dcc20648d462f3519ccd1aaa334b439019a3
Daniel <danrerjsa001@gmail.com>
2019-07-09 17:23:36 +0100

    modified package.json file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist/my-profile'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/myProfile/my-profile'));
+});
+
+app.listen(process.env.PORT || 8080);
